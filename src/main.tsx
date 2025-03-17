05b94c49fbd1091d7c7fb8a01003ccdc1b43e468
llite22 <llite2@yandex.ru>
2025-03-17 17:02:04 +0300

    react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,35 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router';
+import { Menu } from './pages/Menu/Menu';
+import { Error } from './pages/Error/Error';
+import { Cart } from './pages/Cart/Cart';
+import { Layout } from './layout/Menu/Menu';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Layout />,
+		children: [
+			{
+				path: '/',
+				element: <Menu />
+			},
+			{
+				path: '/cart',
+				element: <Cart />
+			}
+		]
+	},
+	{
+		path: '*',
+		element: <Error />
+	}
+]);
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+	<StrictMode>
+		<RouterProvider router={router} />
+	</StrictMode>
+);
